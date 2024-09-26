bfa879bacbde1b0b762698b45abace67c7506270
wenmingming <wenmingming>
2024-09-26 14:59:39 +0800

    Update：更新程序集信息

--- GB28181.NET/obj/Debug/net8.0-windows/GB28181.NET.AssemblyInfo.cs
+++ GB28181.NET/obj/Debug/net8.0-windows/GB28181.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GB28181.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4bcfafe14f7f0828b9e6e59907f7d9a2ececd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+874446200ed58c162b378f24d041a96988e21e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GB28181.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GB28181.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
